ab77809a7f14d6452443c3ccd68c60a7f6735949
Limkon <130622161+Limkon@users.noreply.github.com>
2023-12-01 20:18:16 +0800

    Update login.js

--- login.js
+++ login.js
@@ -2,10 +2,9 @@ const fs = require('fs');
 const puppeteer = require('puppeteer');
 
 (async () => {
-  let browser;  // 将 browser 对象定义在循环外部
+  let browser;
 
   try {
-    // 读取 accounts.json 文件中的 JSON 字符串
     const accountsJson = fs.readFileSync('accounts.json', 'utf-8');
     const accounts = JSON.parse(accountsJson);
 
@@ -17,17 +16,42 @@ const puppeteer = require('puppeteer');
 
       try {
         await page.goto('https://www.alwaysdata.com/login/', { waitUntil: 'load' });
-
-        // 等待页面加载完成
         await page.waitForSelector('#id_login', { timeout: 30000 }).catch(error => console.error(error));
 
-        // 其余部分保持不变
-        // ...
+        const usernameInput = await page.$('#id_login');
+        const passwordInput = await page.$('#id_password');
 
+        if (usernameInput && passwordInput) {
+          await usernameInput.click({ clickCount: 3 });
+          await usernameInput.press('Backspace');
+          await passwordInput.click({ clickCount: 3 });
+          await passwordInput.press('Backspace');
+        }
+
+        await page.type('#id_login', username);
+        await page.type('#id_password', password);
+
+        const loginButton = await page.$('button[type="submit"]');
+        if (loginButton) {
+          await loginButton.click();
+        } else {
+          throw new Error('无法找到登录按钮');
+        }
+
+        await page.waitForNavigation();
+        const isLoggedIn = await page.evaluate(() => {
+          const successElement = document.querySelector('.success-element');
+          return successElement !== null;
+        });
+
+        if (isLoggedIn) {
+          console.log(`账号 ${username} 登录成功！`);
+        } else {
+          console.error(`账号 ${username} 登录失败，请检查账号和密码是否正确。`);
+        }
       } catch (error) {
         console.error(`账号 ${username} 登录时出现错误: ${error}`);
       } finally {
-        // 关闭页面
         await page.close();
       }
     }
@@ -36,7 +60,6 @@ const puppeteer = require('puppeteer');
   } catch (error) {
     console.error(`读取 accounts.json 文件时出现错误: ${error}`);
   } finally {
-    // 关闭浏览器
     if (browser) {
       await browser.close();
     }
